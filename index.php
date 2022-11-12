3b7cf35514ed5d370d41c00eaf2155d67b18a700
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-12 14:05:18 -0700

    bootstrap rows y cols

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Document</title>
+    <?php include_once('layout/head.template.php'); ?>
+</head>
+
+<body>
+
+
+    <div class="container">
+        <!-- <div class="row">
+            <div class="col-2">COL 1</div>
+            <div class="col-10">COL 2</div>
+        </div>
+        <div class="row">
+            <div class="col">COL 1</div>
+            <div class="col">COL 2</div>
+            <div class="col">COL 3</div>
+        </div>
+        <div class="row">
+            <div class="col">COL 1</div>
+            <div class="col">COL 2</div>
+            <div class="col">COL 3</div>
+        </div>
+        <div class="row">
+            <div class="col">COL 1</div>
+            <div class="col">COL 2</div>
+            <div class="col">COL 3</div>
+        </div>
+        <div class="row">
+            <div class="col">COL 1</div>
+            <div class="col">COL 2</div>
+            <div class="col">COL 3</div>
+        </div>
+        <div class="row">
+            <div class="col">COL 1</div>
+            <div class="col">COL 2</div>
+            <div class="col">COL 3</div>
+        </div> -->
+
+        <div class="row row-cols-6">
+            <div class="">Columna</div>
+            <div class="">Columna</div>
+            <div class="">Columna</div>
+            <div class="">Columna</div>
+            <div class="">Columna</div>
+            <div class="">Columna</div>
+            
+        </div>
+    </div>
+
+
+
+
+    <?php include_once('layout/scripts.template.php'); ?>
+
+</body>
+
+</html>
