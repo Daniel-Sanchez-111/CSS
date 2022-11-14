b9337474be8517ca861869f8d6d8b63dd9f3547c
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-14 11:19:52 -0700

    espaciado

--- index.php
+++ index.php
@@ -13,27 +13,32 @@
 
 <body>
 
+        <!-- <div class="row row-cols-6 bg-success bg-opacity-75"">
 
-    <div class="container-fluid">
+        <div class=" col order-2 text-white-50 border border-5 rounded-3 border-dark">Columa</div>
+        <div class="col order-3" style="color:white">Columna</div>
+        <div class="col order-4" style="color:white">Columna</div>
+        <div class="col order-1" style="color:white">Columna5</div>
+        <div class="col order-lg-5 order-xs-1" style="color:white">Columna6</div>
 
-        <div class="row row-cols-6 bg-success bg-opacity-75"">
-            <div class=" col order-2 text-white-50 border border-dark">Columa
-                <div class="row">
-                    <div class="col">Columna anidada</div>
-                    <div class="col">Columna anidada 2</div>
-                </div>
+        </div> -->
+    <!-- <div class="container-fluid">
+
+        
+        
+            <div class=" clearfix">
+                <p class=" bg-primary d-inline">Lorem ipsum dolor</p>
+                <p class="bg-primary d-inline">Lorem ipsum dolor sit.</p>
             </div>
-
-            <div class="col order-3" style="color:white">Columna</div>
-            <div class="col order-4" style="color:white">Columna</div>
-            <div class="col order-1" style="color:white">Columna5</div>
-            <div class="col order-lg-5 order-xs-1" style="color:white">Columna6</div>
-
-        </div>
-    </div>
+ 
+            <div class="ratio ratio-16x9">
+                <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/u9TpV0IzjEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+         </div> -->
 
 
-
+    <button type="button" class="btn-close mt-2 p-3" aria-label="Close"></button>
+    
 
     <?php include_once('layout/scripts.template.php'); ?>
 
