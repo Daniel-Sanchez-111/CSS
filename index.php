fc67bc08a8f0fb860da5d91b9b54703541626f5f
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-14 09:56:24 -0700

    bordes

--- index.php
+++ index.php
@@ -5,50 +5,30 @@
 
     <title>Document</title>
     <?php include_once('layout/head.template.php'); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+
+
 </head>
 
 <body>
 
 
-    <div class="container">
-        <!-- <div class="row">
-            <div class="col-2">COL 1</div>
-            <div class="col-10">COL 2</div>
-        </div>
-        <div class="row">
-            <div class="col">COL 1</div>
-            <div class="col">COL 2</div>
-            <div class="col">COL 3</div>
-        </div>
-        <div class="row">
-            <div class="col">COL 1</div>
-            <div class="col">COL 2</div>
-            <div class="col">COL 3</div>
-        </div>
-        <div class="row">
-            <div class="col">COL 1</div>
-            <div class="col">COL 2</div>
-            <div class="col">COL 3</div>
-        </div>
-        <div class="row">
-            <div class="col">COL 1</div>
-            <div class="col">COL 2</div>
-            <div class="col">COL 3</div>
-        </div>
-        <div class="row">
-            <div class="col">COL 1</div>
-            <div class="col">COL 2</div>
-            <div class="col">COL 3</div>
-        </div> -->
+    <div class="container-fluid">
 
-        <div class="row row-cols-6">
-            <div class="">Columna</div>
-            <div class="">Columna</div>
-            <div class="">Columna</div>
-            <div class="">Columna</div>
-            <div class="">Columna</div>
-            <div class="">Columna</div>
-            
+        <div class="row row-cols-6 bg-success bg-opacity-75"">
+            <div class=" col order-2 text-white-50 border border-dark">Columa
+                <div class="row">
+                    <div class="col">Columna anidada</div>
+                    <div class="col">Columna anidada 2</div>
+                </div>
+            </div>
+
+            <div class="col order-3" style="color:white">Columna</div>
+            <div class="col order-4" style="color:white">Columna</div>
+            <div class="col order-1" style="color:white">Columna5</div>
+            <div class="col order-lg-5 order-xs-1" style="color:white">Columna6</div>
+
         </div>
     </div>
 
