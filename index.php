cff7ce4f6b6825f09b55b0d52d01dce01810bb13
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-15 13:37:53 -0700

    textos

--- index.php
+++ index.php
@@ -12,34 +12,69 @@
             width: 300px;
             height: 300px
         }
+
+        .flex-content {
+            width: 500px;
+            height: 500px;
+        }
+
+        .flex-item {
+            width: 100px;
+            height: 100px;
+        }
     </style>
 
 </head>
 
 <body>
 
-    <!-- <div class="row row-cols-6 bg-success bg-opacity-75"">
+    <blockquote class="blockquote">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eius obcaecati esse, nihil officia nesciunt?</p>
+        <footer class="blockquote-footer">
+            <cite>Miguel luis perez</cite>
+        </footer>
+    </blockquote> 
+
+    <div class="d-flex flex-row d-lg-flex flex-wrap bg-black text-white text-center flex-content">
+    <div class="flex-item p2 bg-info">1</div>
+    <div class="flex-item p2 bg-warning">2</div>
+    <div class="flex-item p2 flex-grow-1 bg-success">3</div>
+    </v>
+
+    <em>Texto EM</em>
+    <abbr title="texto abbr">Texto abbr</abbr>
+    <kbd>Texto kbd</kbd>
+    <var>Texto var</var>
+    <p>You can use the mark tag to <mark>highlight</mark> text.</p>
+    <p><del>This line of text is meant to be treated as deleted text.</del></p>
+    <p><s>This line of text is meant to be treated as no longer accurate.</s></p>
+    <p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
+    <p><u>This line of text will render as underlined.</u></p>
+    <p><small>This line of text is meant to be treated as fine print.</small></p>
+    <p><strong>This line rendered as bold text.</strong></p>
+    <p><em>This line rendered as italicized text.</em></p>
+    <div class="row row-cols-6 bg-success bg-opacity-75"">
 
         <div class=" col order-2 text-white-50 border border-5 rounded-3 border-dark">Columa</div>
-        <div class="col order-3" style="color:white">Columna</div>
-        <div class="col order-4" style="color:white">Columna</div>
-        <div class="col order-1" style="color:white">Columna5</div>
-        <div class="col order-lg-5 order-xs-1" style="color:white">Columna6</div>
+    <div class="col order-3" style="color:white">Columna</div>
+    <div class="col order-4" style="color:white">Columna</div>
+    <div class="col order-1" style="color:white">Columna5</div>
+    <div class="col order-lg-5 order-xs-1" style="color:white">Columna6</div>
 
-        </div> 
-        <div class="container-fluid">
+    </div>
+    <div class="container-fluid">
 
-        
-        
-            <div class=" clearfix">
-                <p class=" bg-primary d-inline">Lorem ipsum dolor</p>
-                <p class="bg-primary d-inline">Lorem ipsum dolor sit.</p>
-            </div>
- 
-            <div class="ratio ratio-16x9">
-                <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/u9TpV0IzjEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-         </div> -->
+
+
+        <div class=" clearfix">
+            <p class=" bg-primary d-inline">Lorem ipsum dolor</p>
+            <p class="bg-primary d-inline">Lorem ipsum dolor sit.</p>
+        </div>
+
+        <div class="ratio ratio-16x9">
+            <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/u9TpV0IzjEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
     <h2>height</h2>
     <div class="bg-black mb-4 d-flex caja">
         <div class="bg-danger mb-2 h-25">Caja 1</div>
@@ -88,19 +123,21 @@
         <div class="row row-cols-1">
             <h2>Celdas</h2>
             <table border class="mb5" style="height: 100px;">
-                    <tbody>
-                        <tr>
-                            <td class="align-baseline">baseline</td>
-                            <td class="align-top">top</td>
-                            <td class="align-middle">middle</td>
-                            <td class="align-bottom">bottom</td>
-                            <td class="align-text-top">text-top</td>
-                            <td class="align-text-bottom">text-bottom</td>
-                        </tr>
-                    </tbody>
+                <tbody>
+                    <tr>
+                        <td class="align-baseline">baseline</td>
+                        <td class="align-top">top</td>
+                        <td class="align-middle">middle</td>
+                        <td class="align-bottom">bottom</td>
+                        <td class="align-text-top">text-top</td>
+                        <td class="align-text-bottom">text-bottom</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
-    </div>
+    </div>  
+
+
 
 
     <?php include_once('layout/scripts.template.php'); ?>
