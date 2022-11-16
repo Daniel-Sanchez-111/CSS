a279a7fc301d6f022a51dbc34c7321adbf22fbbd
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-16 12:08:30 -0700

    carousel

--- index.php
+++ index.php
@@ -21,24 +21,46 @@
 </head>
 
 <body>
-    <!-- BREADCRUMBS -->
+    <!-- Carousel -->
     <div class="container">
         <div class="row row-cols-1">
-            <h1 class="text-center">BREADCRUMBS</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="" class=" text-decoration-none">Inicio</a>
-                    </li>
-                    <li class="breadcrumb-item">
-                        <a href="" class=" text-decoration-none">Libreria</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">
-                        Informacion
-                    </li>
-                </ol>
-            </nav>
+            <h1 class="text-center">Carousel</h1>
+            <div class="carousel slide carousel-fade" id="carousel" data-bs-ride="false">
+                
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./public/img/scooter-01.jpg" alt="" class="d-block w-100">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Texto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, eum.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/img/scooter-02.jpg" alt="" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/img/scooter-03.jpg" alt="" class="d-block w-100">
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
         </div>
+    </div>
     </div>
     <?php include_once('layout/scripts.template.php'); ?>
 
