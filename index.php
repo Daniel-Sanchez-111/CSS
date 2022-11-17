55a7c62b141a2d69dd2e963596a7cce4ee5e2633
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-16 19:26:32 -0700

    paginacion

--- index.php
+++ index.php
@@ -21,46 +21,30 @@
 </head>
 
 <body>
-    <!-- Carousel -->
+    <!-- PAGINACION -->
     <div class="container">
         <div class="row row-cols-1">
-            <h1 class="text-center">List Group</h1>
-
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-                <li class="list-group-item d-flex justify-content-between"> Titulo <span class="badge rounded-pill bg-dark">10</span></li>
-            </ul>
-
-            <h1 class="text-center">Flexbox</h1>
-
-            <div class="list-group list-group-flush col-4" id="grupolista" role="tablist">
-                <a href="#contenido1" data-bs-toggle="list" role="tab" aria-controls="elemento1" id="elemento1" class="list-group-item list-group-item-action active">
-                    Grupo de lista
-                </a>
-                <a href="#contenido2" data-bs-toggle="list" role="tab" aria-controls="elemento2" id="elemento2" class="list-group-item list-group-item-action">
-                    Grupo de lista
-                </a>
-                <a href="#contenido3" data-bs-toggle="list" role="tab" aria-controls="elemento3" id="elemento3" class="list-group-item list-group-item-action">
-                    Grupo de lista
-                </a>
-
-            </div>
-
-            <div class="tab-content col-8" id="contenidoLista">
-                <div class="tab-pane fade active show" role="tabpanel" id="contenido1" aria-labelledby="elemento1">
-                    Contenido 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque.
-                </div>
-                <div class="tab-pane fade" role="tabpanel" id="contenido2" aria-labelledby="elemento2">
-                    Contenido 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sapiente!
-                </div>
-                <div class="tab-pane fade" role="tabpanel" id="contenido3" aria-labelledby="elemento3">
-                    Contenido 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, accusantium.
-                </div>
-            </div>
+            <h1>Paginacion</h1>
+            <nav>
+                <ul class="pagination pagination-lg justify-content-center">
+                    <li class="page-item disabled" disabled><a href="" class="page-link" aria-disabled>
+                            <span aria-hidden="true">&laquo</span>
+                        </a>
+                    </li>
+                    <li class="page-item active"><a href="" class="page-link">
+                            1
+                        </a>
+                    </li>
+                    <li class="page-item"><a href="" class="page-link">
+                            2
+                        </a>
+                    </li>
+                    <li class="page-item"><a href="" class="page-link">
+                            <span aria-hidden="true">&raquo</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
     <?php include_once('layout/scripts.template.php'); ?>
